333f8b432d6673936170ef669b58c09d9284907f
_asdx_ <xasdx@tuta.io>
2018-03-07 16:04:43 +0000

    Implement tests

--- clock.test.js
+++ clock.test.js
@@ -4,15 +4,39 @@ let Clock = require("./clock")
 
 module.exports = {
   "Clock component": {
-    "says hello": () => {
+    "returns the actual time in the format: 'hh:mm:ss'": () => {
       let clock = new Clock()
-      expect(clock.hello()).to.equal("hello")
+      expect(clock.getTime()).to.match(/\d\d:\d\d:\d\d/)
     },
-    "returns the actual time in the format: 'hh:mm:ss'": () => {},
-    "allows setting an initial time": () => {},
-    "updates its state in real time": () => {},
-    "tracks a time that has 70 seconds per minutes and 80 minutes per hours": () => {},
-    "registers observers being called on every update": () => {},
-    "registers and triggers alarms": () => {}
+    "allows setting an initial time": () => {
+      let clock = new Clock("13:37:42")
+      expect(clock.getTime()).to.equal("13:37:42")
+    },
+    "updates its state in real time": done => {
+      let clock = new Clock("13:37:42")
+      setTimeout(() => {
+        expect(clock.getTime()).to.equal("13:37:44")
+        done()
+      }, 2250)
+    },
+    "tracks a time that has 70 seconds per minutes and 80 minutes per hours": done => {
+      let clock = new Clock("13:78:69")
+      setTimeout(() => {
+        expect(clock.getTime()).to.equal("13:79:01")
+        done()
+      }, 2250)
+    },
+    "registers observers being called on every update": done => {
+      let clock = new Clock("13:78:69")
+      let timesOfObservation = 0
+      clock.observe(time => {
+        expect(time).to.match(/\d\d:\d\d:\d\d/)
+        timesOfObservation += 1
+      })
+      setTimeout(() => {
+        expect(timesOfObservation).to.equal(3)
+        done()
+      }, 3250)
+    }
   }
 }
